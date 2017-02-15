f68ad87e60eeccb9c7ef330a400573aa514aec63
Jim Padilla and Kevin Finley <jim+kevin@>
2017-02-15 09:11:44 -0800

    Add paperclip User migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170215170711) do
+ActiveRecord::Schema.define(version: 20170215171104) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,7 +26,11 @@ ActiveRecord::Schema.define(version: 20170215170711) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string   "name"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
 end
