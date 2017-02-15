5a902166b15742f97b47832e7b383452d7e15e12
Jim Padilla and Kevin Finley <jim+kevin@>
2017-02-15 11:31:13 -0800

    Add User upload photo functionality, create Photos Controller

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,2 +1,25 @@
 class PhotosController < ApplicationController
+  def new
+    @user = current_user
+    @photo = @user.photos.new
+  end
+
+  def create
+    @user = current_user
+    @photo = @user.photos.new(photos_params)
+    if @photo.save
+      redirect_to photo_path(@photo)
+    else
+      render :new_user_photo
+    end
+  end
+
+  def show
+    @photo = Photo.find(params[:id])
+  end
+
+  private
+  def photos_params
+  params.require(:photo).permit(:image, :user_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,10 @@ Rails.application.routes.draw do
 
   resources :users, :only => [:show, :update] do
     get 'upload' => 'users#upload_avatar'
+    resources :photos, :except => [:edit, :update]
+  end
+
+  resources :photos, :only => [:show] do
+    resources :comments
   end
 end
